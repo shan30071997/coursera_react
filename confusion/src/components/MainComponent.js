5a9ee1c21aaeeb84421f915d1044127ab88840d8
shan30071997 <shankargawali9665589058@gmail.com>
2020-03-24 21:38:20 +0530

    Single Page Applications Part 2

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -34,6 +34,20 @@ class Main extends Component {
         />
       );
     };
+    const DishWithId = ({ match }) => {
+      return (
+        <DishDetail
+          dish={
+            this.state.dishes.filter(
+              dish => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            comment => comment.dishId === parseInt(match.params.dishId, 10)
+          )}
+        />
+      );
+    };
 
     return (
       <div>
@@ -45,7 +59,9 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
-          <Route exact path="/contactus" component={Contact} />} />
+          <Route path="/menu/:dishId" component={DishWithId} />
+          <Route exact path="/contactus" component={Contact} />
+
           <Redirect to="/home" />
         </Switch>
 

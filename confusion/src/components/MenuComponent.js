90076c3afc467a92250f9273cada3f534c7896de
shan30071997 <shankargawali9665589058@gmail.com>
2020-03-24 13:55:42 +0530

    React coponents assignments

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Dishdetail from "./DishdetailComponent";
 import {
   Card,
   CardImg,
@@ -22,20 +22,6 @@ class Menu extends Component {
     this.setState({ selectedDish: dish });
   }
 
-  renderDish(dish) {
-    if (dish != null)
-      return (
-        <Card>
-          <CardImg top src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    else return <div></div>;
-  }
-
   render() {
     const menu = this.props.dishes.map(dish => {
       return (
@@ -53,11 +39,8 @@ class Menu extends Component {
     return (
       <div className="container">
         <div className="row">{menu}</div>
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            {this.renderDish(this.state.selectedDish)}
-          </div>
-        </div>
+
+        <Dishdetail dish={this.state.selectedDish} />
       </div>
     );
   }

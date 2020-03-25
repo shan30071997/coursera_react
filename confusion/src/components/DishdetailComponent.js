af750124fd3662b554fc8ac9db6db5441d4b03c4
shan30071997 <shankargawali9665589058@gmail.com>
2020-03-25 18:41:36 +0530

    Combining Reducers

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,88 +1,217 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   Card,
   CardImg,
+  CardImgOverlay,
   CardText,
   CardBody,
   CardTitle,
   Breadcrumb,
-  BreadcrumbItem
+  BreadcrumbItem,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Row,
+  Col,
+  Label
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import { Control, LocalForm, Errors } from "react-redux-form";
 
 function RenderDish({ dish }) {
-  if (dish != null)
-    return (
-      <div className="col-12 col-md-5 m-1">
-        <Card>
-          <CardImg top src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      </div>
-    );
-  else return <div></div>;
+  return (
+    <Card>
+      <CardImg width="100%" src={dish.image} alt={dish.name} />
+      <CardBody>
+        <CardTitle>{dish.name}</CardTitle>
+        <CardText>{dish.description}</CardText>
+      </CardBody>
+    </Card>
+  );
 }
 
 function RenderComments({ comments }) {
-  if (comments == null) {
-    return <div></div>;
-  }
-  const cmnts = comments.map(comment => {
+  var commentList = comments.map(comment => {
     return (
       <li key={comment.id}>
-        <p>{comment.comment}</p>
-        <p>
-          --{comment.author},&nbsp;
-          {new Intl.DateTimeFormat("en-US", {
-            year: "numeric",
-            month: "long",
-            day: "2-digit"
-          }).format(new Date(Date.parse(comment.date)))}
-        </p>
+        {comment.comment}
+        <br />
+        <br />
+        -- {comment.author},{" "}
+        {new Intl.DateTimeFormat("en-US", {
+          year: "numeric",
+          month: "short",
+          day: "2-digit"
+        }).format(new Date(Date.parse(comment.date)))}
+        <br />
+        <br />
       </li>
     );
   });
+
   return (
-    <div className="col-12 col-md-5 m-1">
+    <div>
       <h4>Comments</h4>
-      <ul className="list-unstyled">{cmnts}</ul>
+      <ul className="list-unstyled">{commentList}</ul>
+      <CommentForm />
     </div>
   );
 }
 
 const DishDetail = props => {
-  const dish = props.dish;
-  if (dish == null) {
+  if (props.dish) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            <RenderDish dish={props.dish} />
+          </div>
+          <div className="col-12 col-md-5 m-1">
+            <RenderComments comments={props.comments} />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
     return <div></div>;
   }
-
-  return (
-    <div className="container">
-      <div className="row">
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <Link to="/menu">Menu</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-        </Breadcrumb>
-        <div className="col-12">
-          <h3>{props.dish.name}</h3>
-          <hr />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-12 col-md-5 m-1">
-          <RenderDish dish={props.dish} />
-        </div>
-        <div className="col-12 col-md-5 m-1">
-          <RenderComments comments={props.comments} />
-        </div>
-      </div>
-    </div>
-  );
 };
 
 export default DishDetail;
+
+const required = val => val && val.length;
+const maxLength = len => val => !val || val.length <= len;
+const minLength = len => val => val && val.length >= len;
+
+export class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isModalOpen: false
+    };
+
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({ isModalOpen: !this.state.isModalOpen });
+  }
+
+  handleSubmit(values) {
+    this.toggleModal();
+
+    console.log("comment:", values);
+    alert("comment:" + JSON.stringify(values));
+  }
+
+  render() {
+    return (
+      <div>
+        <Button outline onClick={this.toggleModal}>
+          <span className="fa fa-pencil fa-lg"> Submit comment</span>
+        </Button>
+
+        <div className="row row-content">
+          <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+            <ModalHeader toggle={this.toggleModal}> Submit comment</ModalHeader>
+            <ModalBody>
+              <div>
+                <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                  <Row className="form-group">
+                    <Label htmlFor="rating" md={3}>
+                      Rating
+                    </Label>
+                    <Col md={9}>
+                      <Control.select
+                        model=".rating"
+                        name="rating"
+                        className="form-control"
+                      >
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                      </Control.select>
+                    </Col>
+                  </Row>
+
+                  <Row className="form-group">
+                    <Label htmlFor="author" md={3}>
+                      Your name
+                    </Label>
+                    <Col md={9}>
+                      <Control.text
+                        model=".author"
+                        id="author"
+                        name="author"
+                        placeholder="Author"
+                        className="form-control"
+                        validators={{
+                          required,
+                          minLength: minLength(3),
+                          maxLength: maxLength(15)
+                        }}
+                      />
+                      <Errors
+                        className="text-danger"
+                        model=".author"
+                        show="touched"
+                        messages={{
+                          required: "Required",
+                          minLength: "Must be greater than 3 characters",
+                          maxLength: "Must be 15 charaters or less"
+                        }}
+                      />
+                    </Col>
+                  </Row>
+
+                  <Row className="form-group">
+                    <Label htmlFor="feedback" md={3}>
+                      Your feedback
+                    </Label>
+                    <Col md={9}>
+                      <Control.textarea
+                        model=".message"
+                        id="message"
+                        name="message"
+                        rows="6"
+                        className="form-control"
+                        validators={{ required }}
+                      />
+                      <Errors
+                        className="text-danger"
+                        model=".message"
+                        show="touched"
+                        messages={{ required: "Required" }}
+                      />
+                    </Col>
+                  </Row>
+
+                  <Button type="submit" value="submit" color="primary">
+                    Submit
+                  </Button>
+                </LocalForm>
+              </div>
+            </ModalBody>
+          </Modal>
+        </div>
+      </div>
+    );
+  }
+}
--- confusion/src/components/HeaderComponent.js
+++ confusion/src/components/HeaderComponent.js
@@ -41,6 +41,7 @@ class Header extends Component {
       isModalOpen: !this.state.isModalOpen
     });
   }
+
   handleLogin(event) {
     this.toggleModal();
     alert(

f559aa280aaa8eecd3ad3b7614732e6f6aae77af
shan30071997 <shankargawali9665589058@gmail.com>
2020-03-26 12:37:42 +0530

    Fetch Post Comment

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -9,7 +9,7 @@ import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import {
-  addComment,
+  postComment,
   fetchDishes,
   fetchComments,
   fetchPromos
@@ -28,8 +28,8 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = dispatch => ({
-  addComment: (dishId, rating, author, comment) =>
-    dispatch(addComment(dishId, rating, author, comment)),
+  postComment: (dishId, rating, author, comment) =>
+    dispatch(postComment(dishId, rating, author, comment)),
   fetchDishes: () => {
     dispatch(fetchDishes());
   },
@@ -85,7 +85,7 @@ class Main extends Component {
             comment => comment.dishId === parseInt(match.params.dishId, 10)
           )}
           commentsErrMess={this.props.comments.errMess}
-          addComment={this.props.addComment}
+          postComment={this.props.postComment}
         />
       );
     };
